55be3de9cd91a8f539cf9c6e2eb7f03428e7f994
tomz <spirt108@foxmail.com>
2019-07-04 00:27:54 +0800

    other

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -5,6 +5,22 @@
         <nav-menu :navArr="topMainArr" :navType="navType"></nav-menu>
       </div>
     </el-header>
+    <div class="tag-box">
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div class="mask"></div>
+    </div>
     <el-main>
       <transition name="fade">
         <router-view class="view"></router-view>
@@ -51,5 +67,45 @@ export default {
 <style lang="less" scoped>
 #nav {
   margin-bottom: 30px;
+}
+.view {
+  background: #fff;
+}
+.tag-box {
+  width: 100%;
+  height: 30px;
+  border-bottom: 1px solid #ccc;
+  display: flex;
+  flex-direction: row;
+  overflow: hidden;
+  overflow-x: scroll;
+  white-space: nowrap;
+  position: absolute;
+  z-index: -1;
+  .mask {
+    width: 500px;
+    height: 600px;
+    position: absolute;
+    top: 10px;
+    left: 0;
+    background: #f11;
+  }
+  div {
+    flex-shrink: 0;
+    width: 300px;
+    height: 30px;
+    flex-basis: 300px;
+    // display: inline-block;
+  }
+}
+.tag-box:after {
+  display: block;
+  content: " ";
+  width: 100%;
+  height: 600px;
+  position: absolute;
+  top: 10px;
+  left: 0;
+  background: #f11;
 }
 </style>
