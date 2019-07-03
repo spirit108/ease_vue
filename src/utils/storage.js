e04c98068f4f1c73fa917eef32202dc45512980a
zhangxiang <l>
2019-07-03 20:58:01 +0800

    变肤

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,9 +1,9 @@
 /**
- * localStorage操作封装
+ * Storage操作封装
  */
 
-// 保存数据
-function savaStorage(key, params) {
+// 长久保存数据
+function savaLocalStorage(key, params) {
   let _params = "";
   if (params && typeof params === "object") {
     _params = JSON.stringify(params);
@@ -12,47 +12,99 @@ function savaStorage(key, params) {
   }
   localStorage.setItem(key, _params);
 }
-// 删除数据
-function deleteStorage(key) {
+// 指定删除永久数据
+function deleteLocalStorage(key) {
   localStorage.removeItem(key);
 }
-// 获得数据
-function getStorage(key) {
+// 指定获得永久数据
+function getLocalStorage(key) {
   localStorage.getItem(key);
 }
+
+// 临时保存数据
+function savaSessionStorage(key, params) {
+  let _params = "";
+  if (params && typeof params === "object") {
+    _params = JSON.stringify(params);
+  } else if (typeof params === "string") {
+    _params = params;
+  }
+  sessionStorage.setItem(key, _params);
+}
+// 指定删除临时数据
+function deleteSessionStorage(key) {
+  sessionStorage.removeItem(key);
+}
+// 指定获得临时数据
+function getSessionStorage(key) {
+  sessionStorage.getItem(key);
+}
+
 // 保存用户路由
 function savaUserMenuList(menuList) {
-  savaStorage("menuList", menuList);
+  savaLocalStorage("menuList", menuList);
 }
 // 删除用户路由
 function deleteUserMenuList() {
-  deleteStorage("menuList");
+  deleteLocalStorage("menuList");
 }
 // 获取用户路由
 function getUserMenuList() {
-  getStorage("menuList");
+  getLocalStorage("menuList");
 }
-// 保存用户Token
+
+// 永久保存用户Token
 function savaUserToken(token) {
-  savaStorage("token", token);
+  savaLocalStorage("token", token);
 }
-// 获取用户Token
+// 获取永久用户Token
 function getUserToken() {
-  getStorage("token");
+  getLocalStorage("token");
 }
 // 删除用户Token
 function deleteUserToken() {
-  deleteStorage("token");
+  deleteLocalStorage("token");
+}
+
+// 临时保存用户Token
+function savaSessionUserToken(token) {
+  savaSessionStorage("token", token);
+}
+
+// 永久保存用户信息
+function savaUserInfo(userInfo) {
+  savaLocalStorage("userInfo", userInfo);
+}
+// 获取用户信息
+function getUserInfo() {
+  getLocalStorage("userInfo");
+}
+// 删除用户信息
+function deleteUserInfo() {
+  deleteLocalStorage("userInfo");
+}
+
+// 临时保存用户信息
+function savaSessionUserInfo(userInfo) {
+  savaSessionStorage("userInfo", userInfo);
 }
 
 export default {
-  savaStorage: savaStorage,
-  deleteStorage: deleteStorage,
-  getStorage: getStorage,
+  savaLocalStorage: savaLocalStorage,
+  deleteLocalStorage: deleteLocalStorage,
+  getLocalStorage: getLocalStorage,
+  savaSessionStorage: savaSessionStorage,
+  deleteSessionStorage: deleteSessionStorage,
+  getSessionStorage: getSessionStorage,
   savaUserMenuList: savaUserMenuList,
   deleteUserMenuList: deleteUserMenuList,
   getUserMenuList: getUserMenuList,
   savaUserToken: savaUserToken,
   deleteUserToken: deleteUserToken,
-  getUserToken: getUserToken
+  getUserToken: getUserToken,
+  savaSessionUserToken: savaSessionUserToken,
+  savaUserInfo: savaUserInfo,
+  deleteUserInfo: deleteUserInfo,
+  getUserInfo: getUserInfo,
+  savaSessionUserInfo: savaSessionUserInfo
 };
